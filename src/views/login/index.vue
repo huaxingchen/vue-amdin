2354676657df91c7055b345d177de57083e217ca
huaxingchen <2752252852@qq.com>
2019-12-10 18:11:37 +0800

    第一天

---
+++ src/views/login/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="login">
+        fffff
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped lang="scss">
+    #login {
+        height: 100vh;
+        background-color: yellow;
+    }
+</style>
